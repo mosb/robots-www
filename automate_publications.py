293623b6fe5a6d6acd08eca997cc339810201dd5
Michael A Osborne <mosb@robots.ox.ac.uk>
2016-11-22 10:33:16 +0000

    automated publications!

--- automate_publications.py
+++ automate_publications.py
@@ -1,17 +1,30 @@
 import os
 import shutil
 import re
+import subprocess
+
+def copytree(src, dst, symlinks=False, ignore=None):
+    # http://stackoverflow.com/questions/1868714/how-do-i-copy-an-entire-directory-of-files-into-an-existing-directory-using-pyth
+    if not os.path.exists(dst):
+        os.makedirs(dst)
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if os.path.isdir(s):
+            copytree(s, d, symlinks, ignore)
+        else:
+            if not os.path.exists(d) or os.stat(s).st_mtime - os.stat(d).st_mtime > 1:
+                shutil.copy2(s, d)
 
 os.chdir('/Users/michaelosborne/Documents/WWW/MosbWWW/robots-www/')
 
 publication_types = [
-    'Journal'
-    # ,
-    # 'Conference',
-    # 'Preprints',
-    # 'Reports',
-    # 'Workshop',
-    # 'Theses'
+    'Journal',
+    'Conference',
+    'Preprints',
+    'Reports',
+    'Workshop',
+    'Theses'
 ]
 
 # Compile regex we'll use over and over below
@@ -19,7 +32,8 @@ regex = re.compile(r"(?<=file = {).*?(files/)", re.IGNORECASE)
 
 for publication_type in publication_types:
     publication_bib = publication_type + '.bib'
-    publication_bib_path = '_bibliography/' + publication_bib
+    publication_bib_out = publication_type + '_bib.bib'
+    publication_bib_path = '_bibliography/' + publication_bib_out
     shutil.copyfile(
         '_bibliography/' + publication_type + '/' + publication_bib,
         publication_bib_path
@@ -42,10 +56,18 @@ for publication_type in publication_types:
             # Write the file out again
             file.write(line)
 
-    # shutil.copytree( -r _bibliography/Osborne/files/ public/pdf
+    copytree('_bibliography/' + publication_type + '/files', 'public/pdf')
 
-#  _bibliography/osborne.bib
-# rm -rf ./_bibliography/Osborne
-# chmod -R a+rx ./public/pdf
-# jekyll build
+    # process = subprocess.Popen(
+    #     "rm -rf ./_bibliography/" + publication_type,
+    #     stdout=subprocess.PIPE
+    # )
+    # output, error = process.communicate()
+
+
+bashCommand = """
+chmod -R a+rx ./public/pdf
+jekyll build
+"""
 # rsync -avz /Users/michaelosborne/Documents/WWW/MosbWWW/robots-www/_site/ mosb@robots.ox.ac.uk:~/WWW
+process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
--- publications_code.md
+++ publications_code.md
@@ -7,4 +7,26 @@ tags: pubs
 My publications are listed below; Endnote and citation statistics are available at my [Google scholar](http://scholar.google.co.uk/citations?user=iTNcAakAAAAJ)
 profile. Code for selected publications is hosted at [GitHub.](https://github.com/OxfordML)
 
-{% bibliography --file osborne %}
+## Journal papers
+
+{% bibliography --file Journal_bib %}
+
+## Conference papers
+
+{% bibliography --file Conference_bib %}
+
+## Preprints
+
+{% bibliography --file Preprints_bib %}
+
+## Reports
+
+{% bibliography --file Reports_bib %}
+
+## Workshop papers
+
+{% bibliography --file Workshop_bib %}
+
+## Theses
+
+{% bibliography --file Theses_bib %}
